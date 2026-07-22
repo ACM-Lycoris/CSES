f82030db2e802611ed212aea06abb468f2f3f7eb
Lycoris <yeling114514@gmail.com>
2026-07-22 09:20:13 -0400

    Sliding Windows

--- SlidingWindowProblems/B_Minimum.cpp
+++ SlidingWindowProblems/B_Minimum.cpp
@@ -19,32 +19,34 @@ int main()
     ll x, a, b, c;
     cin >> x >> a >> b >> c;
 
-    ll cur=x;
-    ll ans=0;
+    ll cur = x;
+    ll ans = 0;
 
     deque<pair<ll, ll>> mins; // 存储当前窗口使之单调<val,id>
     mins.push_back({x, 1});
 
     for (ll i = 1; i <= n; i++)
     {
-        if(i>1){
-            cur=(a*cur+b)%c;
+        if (i > 1)
+        {
+            cur = (a * cur + b) % c;
         }
-        
-        
-        while(!mins.empty() && mins.back().first>=cur){
+
+        while (!mins.empty() && mins.back().first >= cur)
+        {
             mins.pop_back();
         }
-        mins.push_back({cur,i});
+        mins.push_back({cur, i});
 
-        while(!mins.empty() && mins.front().second <= i-k){
-            //索引超过窗口大小
+        while (!mins.empty() && mins.front().second <= i - k)
+        {
+            // 索引超过窗口大小
             mins.pop_front();
         }
-        if(i>=k){
-            ans^=mins.front().first;
+        if (i >= k)
+        {
+            ans ^= mins.front().first;
         }
-
     }
 
     cout << ans << endl;
